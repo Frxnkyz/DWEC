67b20a77c7fad3b703b228327d55abf3c9f36176
Zarcooo <franromezarco@gmail.com>
2024-09-19 13:04:20 +0200

    Primera Subida DWEC

---
+++ Plantillas/Plantilla01_todoRaiz/funciones.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const resultado01 = document.getElementById("resultado01");
+function mostrar01() {
+    console.log("Estoy mostrando el resultado del ejercicio 01");
+    resultado01.innerHTML = "Estoy mostrando el resultado del ejercicio 01";
+}
+
+const resultado02 = document.getElementById("resultado02");
+function mostrar02() {
+    console.log("Estoy mostrando el resultado del ejercicio 02");
+    resultado02.innerHTML = "Estoy mostrando el resultado del ejercicio 02";
+}
+
+const resultado03 = document.getElementById("resultado03");
+function mostrar03() {
+    console.log("Estoy mostrando el resultado del ejercicio 03");
+    resultado03.innerHTML = "Estoy mostrando el resultado del ejercicio 03";
+}
+
---
+++ Plantillas/Plantilla01_todoRaiz/script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const boton01 = document.getElementById("boton01");
+boton01.addEventListener("click", function () {
+    mostrar01();
+});
+
+const boton02 = document.getElementById("boton02");
+boton02.addEventListener("click", function () {
+    mostrar02();
+});
+
+const boton03 = document.getElementById("boton03");
+boton03.addEventListener("click", function() {
+    mostrar03(), explicacion03();
+});
